acf1a71de690d26cbe8bac14c9694cae44d191b3
Irene890 <munirene20@gmail.com>
2023-06-16 13:40:00 -0700

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 /**
-* Display
-* main function shows start of code
-*/
+ * stdio libary used
+ * main - rerepsents the function takes no arguemens
+ * Description code shows the puts function
+ * in action
+ * it will display the puts
+ */
 	int main(void)
 {
-/*puts function*/
+/**
+ * puts function prints the output
+ * put will dispay the details in quotes
+ */
 	puts("\"Programming is like building a multilingual puzzle");
-/*
-* returns
-* zero
-*/
+/**
+ * Return: zero is successful
+ */
 	return (0);
 }
