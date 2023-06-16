6f8fb48bb9ed4490501d1b456f285d0e704f8243
Irene890 <munirene20@gmail.com>
2023-06-16 02:58:48 +0000

    Puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* Display
+* Main function shows start of code
+* Puts function displays
+* return 0
+*/
+int main(void)
+{
+/**
+ * puts function
+ */
+	puts("\"Programming is like building a multilingual puzzle");
+return(0);
+}
