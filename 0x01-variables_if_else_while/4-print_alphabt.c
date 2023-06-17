cbd8350d7e9163999849c935053e18ab7e8ec5ae
Irene890 <munirene20@gmail.com>
2023-06-17 05:15:28 -0700

    Don't print q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*
+ * main function shows starting
+ * point of code
+ * return funtion shows zero when successful
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 97; n <= 122; n++)
+	{
+		if (n != 101 && n != 113)
+			putchar(n);
+	}
+	putchar('\n');
+	return (0);
+
+}
