86eb5b58b8af9624ecaab8469c6c8d336a94d1be
Irene890 <munirene20@gmail.com>
2023-06-17 09:51:46 -0700

    Prints in lower case

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,7 +2,7 @@
 /**
  * main - describes the starting of the code
  * Return: displays 0 when success
- * Code displays the alphabet in lower case letters
+ * Code displays the alphabet in lower case
  */
 int main(void)
 {
