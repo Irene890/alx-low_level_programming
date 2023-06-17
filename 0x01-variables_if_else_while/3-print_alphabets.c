9e3d45415ef4d06e03b7a9df76b4309a1e07abb9
Irene890 <munirene20@gmail.com>
2023-06-17 04:35:35 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/*
+ * main : description start of code
+ * Display upper and lower case letters
+ * return 0 if successful
+ */
+
+int main(void)
+{
+	int n = 97;
+	int m = 65;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	while (m <= 90)
+	{
+		putchar(m);
+		m++;
+	}
+	putchar('\n');
+	return (0);
+
+}
