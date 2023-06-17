8dbe462ca4cfdfbf5739bc9a132888264084bdf4
Irene890 <munirene20@gmail.com>
2023-06-17 05:30:20 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+ * main function displays entry of code
+ * return displays 0 when successful
+ * Numbers
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0;n <= 9;n++)
+	{
+		printf("%d\n",n);
+	}
+	return (0);
+}
+
+
