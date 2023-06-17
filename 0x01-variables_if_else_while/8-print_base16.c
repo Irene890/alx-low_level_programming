f24e6baa8946e59b084942223c03d93c67b5737b
Irene890 <munirene20@gmail.com>
2023-06-17 10:24:56 -0700

    Print Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
-/*
- *
- *
+/**
+ * main - prints base 16 lower case
+ * Return: 0 when successful
  */
+int main(void)
+{
+	int n = 48;
+	int m = 97;
+
+	while (n <= 57)
+	{
+		putchar(n);
+		n++;
+	}
+	while (m <= 102)
+	{
+		putchar(m);
+		m++;
+	}
+	putchar('\n');
+	return (0);
+}
