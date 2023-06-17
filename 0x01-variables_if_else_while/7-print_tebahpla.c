207d656cf12d87a372c89f792fb486179deb47ab
Irene890 <munirene20@gmail.com>
2023-06-17 07:51:08 -0700

    Reverse alphabet

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -6,12 +6,14 @@
  */
 int main(void)
 {
+/* int main displays lower and upper case letters in reverse */
 	int n = 122;
+
 	while (n >= 97)
-{
+	{
 	putchar(n);
 	n--;
-}
+	}
 	putchar('\n');
 	return (0);
 }
