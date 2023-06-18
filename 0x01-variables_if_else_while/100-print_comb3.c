fac6b88d3307626549655ebc8d3d0a877aca818e
Irene890 <munirene20@gmail.com>
2023-06-17 18:48:10 -0700

    Possible 2 number combinations

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - beginning of code
+ * Return: 0 when successful
+ */
+int main(void)
+{
+int n;
+int m;
+
+for (n = 0; n <= 9 ; n++)
+{
+	for (m = n+1 ; m < 10; m++)
+	{
+			putchar((n%10) + '0');
+			putchar((m%10) + '0');
+			if (n == 8 && m == 9)
+				continue;
+		putchar(',');
+		putchar(' ');
+	}
+}
+putchar('\n');
+return(0);
+}
