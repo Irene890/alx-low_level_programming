e9366f174fb4a5964dd4e38b87a7be81384af18f
Irene890 <munirene20@gmail.com>
2023-06-19 04:16:43 -0700

    101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
-
 /**
   * main - displays the C program code
   * Return: 0 when successful
   */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
