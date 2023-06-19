6f9ce875e4dec6093a47f2c297fe85a5e4a0698b
Irene890 <munirene20@gmail.com>
2023-06-19 03:24:36 -0700

    Hello, prinft funtion

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main is the starting point of the code
- * return : always zero when successful
- */
+  * main - shows the use of printf function
+  * Return: always zero when successful
+  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
