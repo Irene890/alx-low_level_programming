42b89083c360c330f3e9f0cdcf29e1ff18042a07
Irene890 <munirene20@gmail.com>
2023-06-22 06:46:55 -0700

    Remaining days

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * positive_or_negative - entry point for code
+ * Return: zero if successful
+ */
+void positive_or_negative(int i)
+{
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i == 0)
+		printf("%d is zero\n", i);
+	else
+		printf("%d is negative\n", i);
+}
