784f15088f76af21c083f62fadb26312f2445f4f
Irene890 <munirene20@gmail.com>
2023-06-23 03:57:18 -0700

    1 for upper 0 for lower

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - prints our uppercase letters
+ * Return: 1 if uppercase, else 0
+ * @c: is a letter
+ */
+
+int _isupper(int c)
+{
+	if (c == 67)
+		return (1);
+	else
+		return (0);
+}
