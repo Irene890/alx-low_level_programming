4fc4619363e3ef900ae12630a88ab54346d7494f
Irene890 <munirene20@gmail.com>
2023-07-09 11:48:05 -0700

    Argc & Argv Mynameis task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * @argc - argument count
+ * @argv - argument vector
+ * Return: 0 when successful
+ * Write a program that prints its name, followed by a new line.
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n",argv[0]);
+
+	return (0);
+}
+
