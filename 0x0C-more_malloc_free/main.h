945e1de8f09cbf9568f218baf5552c1316760ece
Irene890 <munirene20@gmail.com>
2023-07-14 12:01:30 -0700

    Malloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_HEADRE
-#include MAIN_HEADER
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
 
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
